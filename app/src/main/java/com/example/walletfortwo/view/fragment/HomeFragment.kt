7a8acc6e0733e5cd48db31017446543f5812d612
myu.kato <myu.kato@qnote.co.jp>
2023-01-06 18:12:16 +0900

    ホーム画面とユーザ詳細画面の作成、それに伴う画面遷移の実装

--- app/src/main/java/com/example/walletfortwo/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/walletfortwo/view/fragment/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.walletfortwo.databinding.FragmentHomeBinding
 
 //ホーム画面
@@ -12,7 +13,13 @@ class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return FragmentHomeBinding.inflate(inflater, container, false).apply {
+            userA.container.setOnClickListener {
+                findNavController().navigate(HomeFragmentDirections.homeToUserDetail())
+            }
 
+            userB.container.setOnClickListener {
+                findNavController().navigate(HomeFragmentDirections.homeToUserDetail())
+            }
         }.root
     }
 }
--- app/src/main/java/com/example/walletfortwo/view/fragment/UserDetailFragment.kt
+++ app/src/main/java/com/example/walletfortwo/view/fragment/UserDetailFragment.kt
@@ -5,14 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.walletfortwo.databinding.FragmentUserBinding
+import androidx.navigation.fragment.findNavController
+import com.example.walletfortwo.databinding.FragmentUserDetailBinding
 
 //人の詳細画面
-class UserFragment : Fragment() {
+class UserDetailFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return FragmentUserBinding.inflate(inflater, container, false).apply {
-
+        return FragmentUserDetailBinding.inflate(inflater, container, false).apply {
+            buttonBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
         }.root
     }
 }
