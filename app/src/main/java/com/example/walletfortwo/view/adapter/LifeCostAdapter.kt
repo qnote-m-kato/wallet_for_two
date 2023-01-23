e43602dcf2dc266acf5d345027b8405daa29dbd1
myu.kato <myu.kato@qnote.co.jp>
2023-01-23 13:23:10 +0900

    Adapter準備

---
+++ app/src/main/java/com/example/walletfortwo/view/adapter/LifeCostAdapter.kt
@@ -0,0 +1,70 @@
+package com.example.walletfortwo.view.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.RecyclerView
+import com.example.walletfortwo.R
+import com.example.walletfortwo.databinding.LayoutListItemCostBinding
+import com.example.walletfortwo.model.LifeCost
+
+class LifeCostAdapter(
+    private val list: List<LifeCost>,
+    private val context: Context,
+    private val listener: OnSelectItemListener,
+    private val lifecycleOwner: LifecycleOwner,
+): RecyclerView.Adapter<LifeCostAdapter.ViewHolder>() {
+
+    interface OnSelectItemListener {
+        fun onSelect(item: LifeCost)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = LayoutListItemCostBinding.inflate(LayoutInflater.from(parent.context))
+        binding.lifecycleOwner = lifecycleOwner
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.apply {
+            icArrow.visibility = View.GONE
+            icUserB.visibility = View.GONE
+
+            icUserA.setColorFilter(list[position].user.color)
+            icExpenditure.setImageResource(list[position].expenditureItem.resource)
+            textMoney.text = list[position].cost.toString()
+
+            if (list[position].remarks != null) {
+                icDown.setColorFilter(ContextCompat.getColor(context, R.color.purple_200))
+                icDown.isClickable = true
+            } else {
+                icDown.setColorFilter(ContextCompat.getColor(context, R.color.gray))
+                icDown.isClickable = false
+            }
+
+            icDown.setOnClickListener {
+                icDown.visibility = View.GONE
+                icUp.visibility = View.VISIBLE
+                containerRemarks.visibility = View.VISIBLE
+            }
+
+            icUp.setOnClickListener {
+                icDown.visibility = View.VISIBLE
+                icUp.visibility = View.GONE
+                containerRemarks.visibility = View.GONE
+            }
+
+            container.setOnLongClickListener {
+                listener.onSelect(list[position])
+                return@setOnLongClickListener true
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    class ViewHolder(val binding: LayoutListItemCostBinding) : RecyclerView.ViewHolder(binding.root)
+}
